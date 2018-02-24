5766a92331c4f3184e0a469673a2817220ee0e43
Jonas <oppenheim10@gmail.com>
2018-02-24 11:47:11 -0500

    Initial upload

---
+++ Job.java
@@ -0,0 +1,13 @@
+public class Job {
+
+    public final int startTime;
+    public final int finishTime;
+    public final int value;
+
+    Job(int s, int f, int v) {
+        startTime = s;
+        finishTime = f;
+        value = f;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Job j = new Job(1,2,3);
+    }
+}
